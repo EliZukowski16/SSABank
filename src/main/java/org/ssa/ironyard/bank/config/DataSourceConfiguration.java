f6469ce809be9e045de9cae24ecc8635b71d8e84
Eli Zukowski <eli.zukowski@gmail.com>
2016-09-09 16:34:40 -0400

    more updates

--- src/main/java/org/ssa/ironyard/bank/config/DataSourceConfiguration.java
+++ src/main/java/org/ssa/ironyard/bank/config/DataSourceConfiguration.java
@@ -2,6 +2,9 @@ package org.ssa.ironyard.bank.config;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 import javax.sql.DataSource;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,11 +17,11 @@ public class DataSourceConfiguration
 {
     static final String DATABASE_URL = "jdbc:mysql://localhost/ssa_bank?user=root&password=root&" +
             "useServerPrpStmts=true";
-//    static final Logger LOGGER = LogManager.getLogger(DataSourceConfiguration.class);
+    static final Logger LOGGER = LogManager.getLogger(DataSourceConfiguration.class);
     @Bean
     public DataSource datasource()
     {
-//        LOGGER.debug("YEAH ANnotation based processing is working");
+        LOGGER.debug("YEAH ANnotation based processing is working");
         MysqlDataSource mysqlds = new MysqlDataSource();
         mysqlds.setURL(DATABASE_URL);
         return mysqlds;
