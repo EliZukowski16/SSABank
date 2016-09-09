e8ad8c356f22e9f4f5912990f79977811ff8f036
Eli Zukowski <eli.zukowski@gmail.com>
2016-09-09 14:05:30 -0400

    Still working on tests

--- src/test/java/org/ssa/ironyard/bank/controller/CustomerRestControllerTest.java
+++ src/test/java/org/ssa/ironyard/bank/controller/CustomerRestControllerTest.java
@@ -19,61 +19,61 @@ import org.ssa.ironyard.bank.service.CustomerService;
 public class CustomerRestControllerTest
 {
     
-    CustomerService cs;
-    CustomerRestController controller;
-    List<Customer> allCustomers;
-    
-    @BeforeClass
-    public void setupBefore() throws Exception
-    {
-        BufferedReader reader = null;
-        allCustomers = new ArrayList<>();
-
-        try
-        {
-            reader = Files.newBufferedReader(
-                    Paths.get("C:\\Users\\admin\\workspace\\DatabaseApp\\resources\\MOCK_CUSTOMER_DATA.csv"),
-                    Charset.defaultCharset());
-
-            String line;
-            int id = 1;
-
-            while (null != (line = reader.readLine()))
-            {
-                String[] names = line.split(",");
-                allCustomers.add(new Customer(id, names[0], names[1]));
-                id++;
-            }
-        }
-        catch (IOException iex)
-        {
-            System.err.println(iex);
-            throw iex;
-        }
-        finally
-        {
-            if (null != reader)
-                reader.close();
-        }
-        
-    }
-
-    @Before
-    public void setUp()
-    {
-        this.cs = EasyMock.createNiceMock(CustomerService.class);
-        this.controller = new CustomerRestController();
-    }
-
-
-    @Test
-    public void viewAllCustomers()
-    {
-        EasyMock.expect(this.cs.read()).andReturn(allCustomers);
-        EasyMock.replay(this.cs);
-        
-        ResponseEntity<List<Customer>> customers = this.controller.allCustomers();
-
-    }
+//    CustomerService cs;
+//    CustomerRestController controller;
+//    List<Customer> allCustomers;
+//    
+//    @BeforeClass
+//    public void setupBefore() throws Exception
+//    {
+//        BufferedReader reader = null;
+//        allCustomers = new ArrayList<>();
+//
+//        try
+//        {
+//            reader = Files.newBufferedReader(
+//                    Paths.get("C:\\Users\\admin\\workspace\\DatabaseApp\\resources\\MOCK_CUSTOMER_DATA.csv"),
+//                    Charset.defaultCharset());
+//
+//            String line;
+//            int id = 1;
+//
+//            while (null != (line = reader.readLine()))
+//            {
+//                String[] names = line.split(",");
+//                allCustomers.add(new Customer(id, names[0], names[1]));
+//                id++;
+//            }
+//        }
+//        catch (IOException iex)
+//        {
+//            System.err.println(iex);
+//            throw iex;
+//        }
+//        finally
+//        {
+//            if (null != reader)
+//                reader.close();
+//        }
+//        
+//    }
+//
+//    @Before
+//    public void setUp()
+//    {
+//        this.cs = EasyMock.createNiceMock(CustomerService.class);
+//        this.controller = new CustomerRestController();
+//    }
+//
+//
+//    @Test
+//    public void viewAllCustomers()
+//    {
+//        EasyMock.expect(this.cs.read()).andReturn(allCustomers);
+//        EasyMock.replay(this.cs);
+//        
+//        ResponseEntity<List<Customer>> customers = this.controller.allCustomers();
+//
+//    }
 
 }
