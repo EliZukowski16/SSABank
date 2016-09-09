cbf8243132cf79456a283768d435369ff16d3818
theshahofny <s90amii@gmail.com>
2016-09-09 13:14:52 -0400

    Add files via upload

--- src/main/java/org/ssa/ironyard/bank/service/CustomerServiceImpl.java
+++ src/main/java/org/ssa/ironyard/bank/service/CustomerServiceImpl.java
@@ -1,27 +1,20 @@
-package org.ssa.ironyard.bank.service;
+package org.ssa.ironyard.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.ssa.ironyard.bank.dao.CustomerDAO;
-import org.ssa.ironyard.bank.model.Customer;
+import org.ssa.ironyard.customer.dao.CustomerDAO;
+import org.ssa.ironyard.customer.model.Customer;
 
-@Component
 public class CustomerServiceImpl implements CustomerService {
 
-    @Autowired
 	CustomerDAO customerDao;
 	
 	@Override
 	public List<Customer> read() {
 		List<Customer> customers = new ArrayList<>();
 		customers.addAll(customerDao.read());
-		return customers.stream()
-                .sorted((c1, c2) -> c1.getFirstName().compareTo(c2.getFirstName()))
-                .sorted((c1, c2) -> c1.getLastName().compareTo(c2.getLastName())).collect(Collectors.toList());
+		return customers;
 	}
 
 	@Override
@@ -33,6 +26,12 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public Customer insert(Customer customer) {
 		return customerDao.insert(customer);
+	}
+
+
+	@Override
+	public Customer update(Customer customer) {
+		return customerDao.update(customer);
 	}
 
 }
