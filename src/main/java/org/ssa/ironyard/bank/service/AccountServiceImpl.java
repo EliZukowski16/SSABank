8c8594b0de2bd24b5e33f9cb09215dd49c48b252
Eli Zukowski <eli.zukowski@gmail.com>
2016-09-15 15:20:18 -0400

    Added transfer error checking

--- src/main/java/org/ssa/ironyard/bank/service/AccountServiceImpl.java
+++ src/main/java/org/ssa/ironyard/bank/service/AccountServiceImpl.java
@@ -10,57 +10,64 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import org.ssa.ironyard.bank.dao.AccountDAOEager;
 import org.ssa.ironyard.bank.model.Account;
 
 @Component
-public class AccountServiceImpl implements AccountService {
+public class AccountServiceImpl implements AccountService
+{
 
     Logger LOGGER = LogManager.getLogger(AccountServiceImpl.class);
-    
+
     @Autowired
-	AccountDAOEager accountDao;
-	
-	@Override
-	@Transactional
-	public List<Account> readUser(int userID) {
-		List<Account> accounts = new ArrayList<>();
-		accounts.addAll(accountDao.readUser(userID));
-		return accounts;
-	}
+    AccountDAOEager accountDao;
 
-	@Override
-	@Transactional
-	public Account read(int accountID) {
-		return accountDao.read(accountID);
-	}
+    @Override
+    @Transactional
+    public List<Account> readUser(int userID)
+    {
+        List<Account> accounts = new ArrayList<>();
+        accounts.addAll(accountDao.readUser(userID));
+        return accounts;
+    }
 
-	@Override
-	@Transactional
-	public Account insert(Account account) {
-		return accountDao.insert(account);
-	}
+    @Override
+    @Transactional
+    public Account read(int accountID)
+    {
+        return accountDao.read(accountID);
+    }
 
-	@Override
-	@Transactional
-	public Account deposit(int accountID, BigDecimal amount) {
-		Account a1 = accountDao.read(accountID);
-		LOGGER.info("Performing deposit on Account {}",a1.getId());
-		a1.adjustBalance("deposit", amount);
-		LOGGER.info("New balance is {}", a1.getBalance());
-		return accountDao.update(a1);
-	}
+    @Override
+    @Transactional
+    public Account insert(Account account)
+    {
+        return accountDao.insert(account);
+    }
 
-	@Override
-	@Transactional
-	public Account withdraw(int accountID, BigDecimal amount) {
-		Account a1 = accountDao.read(accountID);
-		LOGGER.info("Performing withdraw on Account {}",a1.getId());
-		a1.adjustBalance("withdraw", amount);
-	    LOGGER.info("New balance is {}",a1.getBalance());
-		return accountDao.update(a1);
-	}
+    @Override
+    @Transactional
+    public Account deposit(int accountID, BigDecimal amount)
+    {
+        Account a1 = accountDao.read(accountID);
+        LOGGER.info("Performing deposit on Account {}", a1.getId());
+        a1.adjustBalance("deposit", amount);
+        LOGGER.info("New balance is {}", a1.getBalance());
+        return accountDao.update(a1);
+    }
+
+    @Override
+    @Transactional
+    public Account withdraw(int accountID, BigDecimal amount)
+    {
+        Account a1 = accountDao.read(accountID);
+        LOGGER.info("Performing withdraw on Account {}", a1.getId());
+        a1.adjustBalance("withdraw", amount);
+        LOGGER.info("New balance is {}", a1.getBalance());
+        return accountDao.update(a1);
+    }
 
     @Override
     @Transactional
@@ -72,28 +79,39 @@ public class AccountServiceImpl implements AccountService {
     @Override
     @Transactional
     public Map<String, Account> transfer(Integer customerID, Integer sourceAccountID, Integer targetAccountID,
-            BigDecimal amount) {
+            BigDecimal amount)
+    {
         List<Account> customerAccounts = accountDao.readUser(customerID);
         Account sourceAccount = null, targetAccount = null;
-        
+
         Map<String, Account> transferAccounts = new HashMap<>();
-        
-        for(Account a : customerAccounts) {
-            if(a.getId() == sourceAccountID)
+
+        for (Account a : customerAccounts)
+        {
+            if (a.getId() == sourceAccountID)
                 sourceAccount = a;
-            if(a.getId() == targetAccountID)
+            if (a.getId() == targetAccountID)
                 targetAccount = a;
-            if((sourceAccount != null) && (targetAccount != null))
+            if ((sourceAccount != null) && (targetAccount != null))
                 break;
         }
-        
+
+        if (sourceAccount.getBalance().subtract(amount).compareTo(BigDecimal.valueOf(-9999999999.99)) < 0
+                || targetAccount.getBalance().add(amount).compareTo(BigDecimal.valueOf(-9999999999.99)) > 0)
+        {
+            transferAccounts.put("source", null);
+            transferAccounts.put("target", null);
+            return transferAccounts;
+        }
+
         sourceAccount.adjustBalance("withdraw", amount);
         targetAccount.adjustBalance("deposit", amount);
-        
+
         transferAccounts.put("source", accountDao.update(sourceAccount));
         transferAccounts.put("target", accountDao.update(targetAccount));
-        
+
         return transferAccounts;
+
     }
 
 }
